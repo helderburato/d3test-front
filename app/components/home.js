c3e6387f98e3726e03b45f8586be92da5a88da51
Helder Burato Berto <helder.burato@gmail.com>
2018-02-06 15:56:45 -0200

    - Add reset css; - Add collumns to form and details payment.

--- app/components/home.js
+++ app/components/home.js
@@ -11,6 +11,15 @@ const Home = props =>
       <h1>Cadastro</h1>
 
       <Steps active="4" />
+
+      <div className="content__wrapper">
+        <div className="block-form col">
+          <h2>Pagamento</h2>
+        </div>
+        <div className="block-detail col">
+          <h2>Detalhes da Compra</h2>
+        </div>
+      </div>
     </div>
 
     <Footer />
